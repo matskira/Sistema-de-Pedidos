86895c823ffc1a6440bbf34bccf25f05ab8b6926
matskira <matheus.poda@gmail.com>
2020-06-08 08:53:41 -0300

    Criando Operação de Instância

--- src/main/java/com/mpoda/cursomc/CursomcApplication.java
+++ src/main/java/com/mpoda/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.mpoda.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mpoda.cursomc.domain.Categoria;
+import com.mpoda.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
