5b20a7a95d26708b7b079c43f875c4e88282d3f8
matskira <matheus.poda@gmail.com>
2020-06-08 10:58:22 -0300

    Adicionado os domínios Estado e Cidades

--- src/main/java/com/mpoda/cursomc/CursomcApplication.java
+++ src/main/java/com/mpoda/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mpoda.cursomc.domain.Categoria;
+import com.mpoda.cursomc.domain.Cidade;
+import com.mpoda.cursomc.domain.Estado;
 import com.mpoda.cursomc.domain.Produto;
 import com.mpoda.cursomc.repositories.CategoriaRepository;
+import com.mpoda.cursomc.repositories.CidadeRepository;
+import com.mpoda.cursomc.repositories.EstadoRepository;
 import com.mpoda.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -24,6 +28,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -44,6 +54,19 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
+		
+		Estado estado1 = new Estado(null, "Minas Gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+		
+		Cidade cidade1 = new Cidade(null, "Uberlandia", estado1);
+		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
+		Cidade cidade3 = new Cidade(null, "Campinas", estado1);
+		
+		estado1.getCidades().addAll(Arrays.asList(cidade1));
+		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
+		
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
 	}
 
 }
