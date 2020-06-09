2802534e63e1534fd01e20031a9afa9e5b33609e
matskira <matheus.poda@gmail.com>
2020-06-09 08:54:17 -0300

    Endpoint clientes

---
+++ src/main/java/com/mpoda/cursomc/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.mpoda.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mpoda.cursomc.domain.Cliente;
+import com.mpoda.cursomc.services.ClienteService;
+
+@RestController //Um controle Rest
+@RequestMapping(value = "/clientes") //Endpoint REST
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value= "/{id}",method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = service.find(id);	
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
